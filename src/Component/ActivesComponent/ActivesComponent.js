0779cc32dafe09ecb2356314b60508b14b03e52f
ngxuanthang1111 <ngxuanthang1111@gmail.com>
2019-09-23 11:12:23 +0700

    load list experience

--- src/Component/ActivesComponent/ActivesComponent.js
+++ src/Component/ActivesComponent/ActivesComponent.js
@@ -2,6 +2,13 @@ import React, {Component} from 'react';
 import './ActivesComponent.css';
 
 class ActivesComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
     render() {
         return (
             <div className= "wrapper__component--actives">
--- src/Component/EducationComponent/EducationComponent.js
+++ src/Component/EducationComponent/EducationComponent.js
@@ -2,19 +2,31 @@ import React, {Component} from 'react';
 import './EducationComponent.css';
 
 class EducationComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: "học vấn",
+            startTime: '09/2014',
+            endTime: '06/2018',
+            university: 'Duy Tan University',
+            major: 'Software Technology',
+            gpa: '3.2'
+        }
+    }
+
     render() {
         return (
             <div className="wrapper__component--education">
                 <h3 className="component__education--title">học vấn</h3>
                 <div className="d-flex component__education--content">
                     <div className="component__education--time">
-                        <p>start <span>-</span> end</p>
+                        <p>{this.state.startTime} <span>-</span> {this.state.endTime}</p>
                     </div>
                     <div className="component__education--result">
                         <ul>
-                            <li>Đại Hoc ABC</li>
-                            <li>Chuyên ngành: Demo Web</li>
-                            <li>GPA: 3.8/4</li>
+                            <li>{this.state.university}</li>
+                            <li>Chuyên ngành: {this.state.major}</li>
+                            <li>GPA: {this.state.gpa}/4</li>
                         </ul>
                     </div>
                 </div>
